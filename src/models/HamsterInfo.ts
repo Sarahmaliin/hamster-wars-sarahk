f0c49e06f55b296daa7930ccb6e10a0e910ca9c3
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-10-27 13:50:27 +0200

    fixed errors w logo.png

---
+++ src/models/HamsterInfo.ts
@@ -0,0 +1,11 @@
+export interface HamsterInfo{
+    name: string,
+    age: number,
+    defeats: number,
+    favFood: string,
+    games: number,
+    id: any,
+    imgName: string,
+    loves: string,
+    wins: number
+}
