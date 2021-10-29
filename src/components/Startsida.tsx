3362c2ddfe3dd0d186a0125cecc1ffeb26547d9e
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-10-29 14:52:22 +0200

    started on opening text

--- src/components/Startsida.tsx
+++ src/components/Startsida.tsx
@@ -1,7 +1,23 @@
 
 const Startsida = () =>{
     return(
-        <h1>startsida</h1>
+        <section>
+            <h1>Välkommen till Hamster Wars</h1>
+            <article>
+                Det här är ett spel för unga som gamla, djurälskare men framförallt hamsterälskare. 
+                <br/>
+                Hur spelet går till?
+                <br/>
+                I övre högra hörnet har du din meny. Just nu befinner du dig på startsidan. 
+                I nästa flik som vi kallar "Tävla" kommer två hamstrar att slumpas fram. Det är då upp till dig att rösta vilken hamster som är sötast. När du har rösat sparas status i vårt system och den/de hamster/hamstrar med högst betyg presenteras här på startsidan. 
+                <br/>
+                Du har även möjlighet att ta bort och lägga till dina egna hamstrar i tävlingen. 
+            </article>
+            <aside>
+                1:a platsen just nu har xxx med y poäng!
+            </aside>
+        </section>
+        
     )
 }
 
