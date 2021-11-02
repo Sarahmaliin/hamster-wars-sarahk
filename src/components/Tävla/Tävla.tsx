438b24bffb89b105f0c486de3ac97bf881e8e82f
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-11-02 16:00:20 +0100

    started on compete

--- src/components/Tävla/Tävla.tsx
+++ src/components/Tävla/Tävla.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
-import { HamsterInfo } from "../../models/HamsterInfo"
 import FirstHamster from "./FirstHamster"
 import SecondHamster from "./SecondHamster"
 
 //Visa två slumpade hamstrar. Låt användaren välja den sötaste. Visa resultatet och initiera nästa match.
 const Tävla = () =>{
-    
+
     return(
         <section>
             <h1>Hamster vs Hamster</h1>
