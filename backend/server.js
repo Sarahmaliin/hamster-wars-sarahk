397663be34793efee6fa871a726cda4ec8f7680b
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-11-04 09:24:18 +0100

    rearrange port

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/public'))
 app.use('/hamsters', hamsterRouter)
 
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + 'index.html')
+	res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () =>{
