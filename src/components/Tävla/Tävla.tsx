e7165f24b812120873408c30d268c2f2aed58c10
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-11-04 13:12:06 +0100

    logging winner + loser

--- src/components/Tävla/Tävla.tsx
+++ src/components/Tävla/Tävla.tsx
@@ -14,8 +14,10 @@ import { HamsterInfo } from "../../models/HamsterInfo"
 const Tävla = () =>{
     const [saveRandomOne, setSaveRandomOne] = useState<HamsterInfo[] | null>(null)
     const [saveRandomTwo, setSaveRandomTwo] = useState<HamsterInfo[] | null>(null)
-    const [valueOne, setValueOne] = useState(0)
-    const [valueTwo, setValueTwo] = useState(0)
+    const [winnerOne, setWinnerOne] = useState<Number>(0)
+    const [winnerTwo, setWinnerTwo] = useState<Number>(0)
+    const [loserOne, setLoserOne] = useState<Number>(0)
+    const [loserTwo, setLoserTwo] = useState<Number>(0)
     const [visible, setVisible] = useState<boolean>(false)
     
 
@@ -27,59 +29,94 @@ const Tävla = () =>{
         sendRequestTwo(setSaveRandomTwo)
     }, []) 
 
-    // useEffect(() => {
-    //     setValue(JSON.parse(window.localStorage.getItem('value')|| '{}')); //sparar value vilket innehåller hamstrarnas vinster, uppdaterar varje
-    //   }, []);
-    
-    //   useEffect(() => {
-    //     window.localStorage.setItem('value', value.toString());
-    //   }, [value]);
+    useEffect(() =>{
+            const voting = {wins: winnerOne}
+            if(saveRandomOne){
+                fetch(`/hamsters/${saveRandomOne[id]}`, {
+             method: 'PUT',
+             headers: {
+               'Content-Type': 'application/json',
+             },
+             body: JSON.stringify(voting),
+           })
+          setVisible(!visible)
+          console.log(voting)
+            }
+             
+    }, [winnerOne])
 
-    //få in så uppdaterar vald hamster
-    //få in värde i overlay
-    //gör loser funktionen
+    useEffect(() =>{
+        const voting2 = {wins: winnerTwo}
+        if(saveRandomTwo){
+            fetch(`/hamsters/${saveRandomTwo[id]}`, {
+         method: 'PUT',
+         headers: {
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(voting2),
+       })
+      setVisible(!visible)
+      console.log(voting2)
+        }
+         
+}, [winnerTwo])
 
-    
+useEffect(() =>{
+    const votingLose1 = {defeats: loserOne}
+    if(saveRandomOne){
+        fetch(`/hamsters/${saveRandomOne[id]}`, {
+     method: 'PUT',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(votingLose1),
+   })
+  setVisible(!visible)
+  console.log(votingLose1)
+    }
+     
+}, [loserOne])
+
+useEffect(() =>{
+    const votingLose2 = {defeats: loserTwo}
+    if(saveRandomTwo){
+        fetch(`/hamsters/${saveRandomTwo[id]}`, {
+     method: 'PUT',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(votingLose2),
+   })
+  setVisible(!visible)
+  console.log(votingLose2)
+    }
+     
+}, [loserTwo])
+
     async function Vote(HamsterId: HamsterInfo){
         console.log(HamsterId)
-            if(saveRandomOne && HamsterId === saveRandomOne[id]){
-            console.log(saveRandomOne[id])
-            console.log('winner is hamster one')
-             await setValueOne(Number(valueOne) + 1)
-             const voting = {wins: valueOne}
-
-             await fetch(`/hamsters/${saveRandomOne[id]}`, {
-             method: 'PUT',
-             headers: {
-               'Content-Type': 'application/json',
-             },
-             body: JSON.stringify(voting),
-           })
-          setVisible(!visible)
-          console.log(voting)
+        if(saveRandomOne && saveRandomTwo){
+            if(HamsterId === saveRandomOne[id]){
+                console.log(saveRandomOne[id])
+                console.log('winner is hamster one')
+                const updateNumber = Number(saveRandomOne[wins]) + 1
+                setWinnerOne(updateNumber)
+                const updateLoser2 = Number(saveRandomTwo[defeats]) + 1
+                setLoserTwo(updateLoser2)
+                }
+            if(HamsterId === saveRandomTwo[id]){
+                console.log('winner two')
+                console.log(saveRandomTwo[id])
+                const updateNumber2 = Number(saveRandomTwo[wins]) + 1
+                setWinnerTwo(updateNumber2)
+                console.log('loser 1')
+                const updateLoser1 = Number(saveRandomOne[defeats]) + 1
+                setLoserOne(updateLoser1)
+                
+            }
         }
-        else if(saveRandomTwo && HamsterId === saveRandomTwo[id]){
-            console.log('winner is number two')
-            await setValueTwo(Number(valueTwo) + 1)
-             const voting = {wins: valueTwo}
-
-             await fetch(`/hamsters/${saveRandomTwo[id]}`, {
-             method: 'PUT',
-             headers: {
-               'Content-Type': 'application/json',
-             },
-             body: JSON.stringify(voting),
-           })
-          setVisible(!visible)
-          console.log(voting)
-        }
-        }  
-        
-
-    const RefreshPage = () =>{
-        window.location.reload();
+            
     }
-
 async function sendRequestOne(saveData: any){
     const response = await fetch ('/hamsters/random')
     const data = await response.json()
@@ -121,19 +158,19 @@ async function sendRequestTwo(saveD: any){
                 <h1>Vinnare: </h1>
                 <img src={`../../img/${saveRandomOne[imgName]}`} alt="hamster" />
                 <p>Namn: {saveRandomOne[name]}</p> 
-                <p>Vinster: {saveRandomOne[wins]}</p>
-                <p>Förluster: {saveRandomOne[defeats]}</p>
+                <p>Vinster: {winnerOne}</p>
+                <p>Förluster: {loserOne}</p>
 
             </li>
             <li>
             <h1>Förlorare</h1>
             <img src={`../../img/${saveRandomOne[imgName]}`} alt="hamster" />
             <p>Namn: {saveRandomTwo[name]}</p> 
-            <p>Vinster: {saveRandomTwo[wins]}</p>
-            <p>Förluster: {saveRandomTwo[defeats]}</p>
+            <p>Vinster: {winnerTwo}</p>
+            <p>Förluster: {loserTwo}</p>
 
         </li>
-            <button onClick={() => RefreshPage()}>Starta nytt spel</button>
+            <button onClick={() => window.location.reload()}>Starta nytt spel</button>
             </ul>
             : null}
             
