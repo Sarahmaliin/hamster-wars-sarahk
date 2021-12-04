061bc4e211a3ed651ad52b59d3408bc29e543e47
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-12-04 14:50:36 +0100

    löst errors med formulär

--- src/components/galleri/FormHamster.tsx
+++ src/components/galleri/FormHamster.tsx
@@ -6,11 +6,12 @@ const FormHamster = (props: any) =>{
 
   const [ showForm, setShowForm ] = useState<boolean>(false)
     const [hamsterName] = useState('')
-    const [age, setAge] = useState(0)
+    const [age] = useState(0)
     const [hamsterFood] = useState('')
     const [hamsterLove] = useState('')
     const [hamsterImg] = useState('')
     const [allOkey, setAllOkey] = useState<boolean>() 
+    const [removeFormValues, setRemoveFormValues] = useState<boolean>()
     const [ errorName, setErrorName ] = useState<string>('')
     const [ errorAge, setErrorAge ] = useState<string>('')
     const [ errorFood, setErrorFood ] = useState<string>('')
@@ -18,6 +19,7 @@ const FormHamster = (props: any) =>{
     const [ errorImg, setErrorImg ] = useState<string>('')
     const [ formError, setFormError ] = useState<string>('')
     const [errorMsg, setErrorMsg ] = useState<string>('')
+
 
 
     const [newHamster, setNewHamster] = useState({
@@ -37,19 +39,29 @@ const FormHamster = (props: any) =>{
     const favFoodInput = (document.querySelector('.favFood') as HTMLInputElement)
     const lovesInput = (document.querySelector('.loves') as HTMLInputElement)
     const imgNameInput = (document.querySelector('.imgName') as HTMLInputElement)
+    
     let regExp = /^[a-öA-Ö]*$/;
     let ValidNmb = /[0-9]/
 
     useEffect(() =>{
         if(allOkey === true){
             saveForm()
-            
         }  
          if(allOkey === false){
             setFormError('Var god se felmeddelanden nedan')
         } 
         // eslint-disable-next-line
     }, [allOkey])
+
+    useEffect(() =>{
+        newHamster.name = ''
+        newHamster.age = 0
+        newHamster.favFood = ''
+        newHamster.loves = ''
+        newHamster.imgName = '' 
+
+        // eslint-disable-next-line
+    }, [removeFormValues])
 
     async function saveForm () {
         const res = await fetch('/hamsters', {
@@ -65,11 +77,12 @@ const FormHamster = (props: any) =>{
         let data = await res.json()
         setNewHamster(data)
         reloadAfterSubmit()
+        
     }
 
     const ValidateName = ()=>{
+        CheckEmpty(lovesInput)
         const hamsterValidateName = nameInput.value.trim()
-        
         return hamsterValidateName.length >= 2  && !hamsterValidateName.includes('-') && !hamsterValidateName.includes('.') && regExp.test(hamsterValidateName)
     }
 
@@ -85,62 +98,87 @@ const FormHamster = (props: any) =>{
     }
 
     const ValidateAge = () =>{
+        CheckEmpty(ageInput)
         const hamsterValidateAge = ageInput.value.trim()
         let nmb = parseInt(hamsterValidateAge)
         return nmb > 2 && nmb < 100 && !hamsterValidateAge.includes(',') && !hamsterValidateAge.includes('.') && ValidNmb.test(hamsterValidateAge)       
     }
 
     const ValidateFood = () =>{
+        CheckEmpty(favFoodInput)
         const hamsterValidateFavFood = favFoodInput.value.trim()
         return hamsterValidateFavFood.length >= 2 && regExp.test(hamsterValidateFavFood)
     }
 
 
     const ValidateLove = () =>{
+        CheckEmpty(lovesInput)
         const hamsterValidateLove = lovesInput.value.trim()
         return hamsterValidateLove.length >= 2 && regExp.test(hamsterValidateLove)
     }
 
     const ValidateImg = () =>{
+        CheckEmpty(imgNameInput)
         const hamsterValidateImg = imgNameInput.value.trim() 
         return hamsterValidateImg.length >= 2 && hamsterValidateImg.includes('-') && hamsterValidateImg.includes('.') 
     }
 
     const SaveInput = (event: React.FormEvent<HTMLFormElement>) =>{
-        
+
         event.preventDefault()
+        
         if(!ValidateName()) {
+            newHamster.name = '';
             setErrorName('Nedanstående namn-fält är inkorrekt. Se till så att namnet inte är tomt, är över 2 bokstäver och inte innehåller specialtecken')
         }
 
         if(!ValidateAge() && !validateNaN()) {
+            newHamster.age = 0;
             setErrorAge('Nedanstående ålder-fält är inkorrekt. Se till så att åldern är över 2, under 100 och inte inkluderar decimaler')
         }
 
         if(!ValidateFood()) {
+            newHamster.favFood = '';
             setErrorFood('Nedanstående favoritmat-fält är inkorrekt, se till så att namnet är över 2 bokstäver och inte innehåller specialtecken')
         }
 
         if(!ValidateLove()) {
+            newHamster.loves = '';
             setErrorLoves('Nedanstående älskar-fält är inkorrekt, se till så att namnet är över 2 bokstäver och inte innehåller specialtecken')
         }
 
         if(!ValidateImg()) {
+            newHamster.imgName = ''
             setErrorImg('Nedanstående bild-fält är inkorrekt, se till så att bildurl är över 2 bokstäver, inkluderar bindestreck och punkt')
-            
         }
 
-
-
         setAllOkey(ValidateName() && validateNaN() && ValidateAge() && ValidateFood() && ValidateLove() && ValidateImg())
+        
     }
 
+
     const reloadAfterSubmit = () =>{
-        props.formClosed(true)
+        props.formClosed(true)  
+        setRemoveFormValues(true)
         setShowForm(false)
+        setFormError('')
+        setErrorName('')
+        setErrorAge('')
+        setErrorFood('')
+        setErrorLoves('')
+        setErrorImg('')
+             
+    }
+
+    const CheckEmpty = (input: any) =>{
+        if(input === null || undefined){
+            console.log('wrong')
+            return false
+        }
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        event.preventDefault()
         setNewHamster({...newHamster, [event.target.name]: event.target.value.trim()})  //trim removes whitespace
     }  
 
@@ -165,7 +203,7 @@ const FormHamster = (props: any) =>{
                     <h3 className='errorMessages'>{errorImg}</h3>
                     <input className='imgName' onChange={handleChange} name='imgName' value={newHamster.imgName} type="text" placeholder='Bildurl' />
                     
-                <button  type="submit">Lägg till</button>
+                <button id="submitBtn" type="submit">Lägg till</button>
                 </section>
                 
             </form>: <article className="addHamster">
